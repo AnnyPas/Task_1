493f5440738516d721fd1c3261682b3e35fa7dc1
AnnPas <janeair1568@gmail.com>
2026-09-18 17:21:39 +0500

    Make unit tests atomic

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,16 +1,18 @@
 package praktikum;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BunTest {
 
     @Test
-    void getNameAndPriceReturnsBunData() {
+    void getNameReturnsBunData() {
         Bun bun = new Bun("black bun", 100.0f);
-
         assertEquals("black bun", bun.getName());
+    }
+    @Test
+    void getPriceReturnsBunData() {
+        Bun bun = new Bun("black bun", 100.0f);
         assertEquals(100.0f, bun.getPrice(), 0.001);
     }
 }
